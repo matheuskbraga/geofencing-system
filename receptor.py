13c6599f43ad534ca4dfbc79fee2fea4a0d3e607
Matheus Braga <matheuskbraga9@gmail.com>
2025-04-25 16:08:55 -0300

    Primeiro commit: Adicionando código de GPS para o ESP8266

--- receptor.py
+++ receptor.py
@@ -2,7 +2,7 @@ import socket
 
 # Defina o IP e a porta do servidor (deve ser o mesmo configurado no ESP8266)
 local_ip = "0.0.0.0"  # A escuta de todas as interfaces
-local_port = 8888
+local_port = 8888     #
 
 # Criar o socket UDP
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
